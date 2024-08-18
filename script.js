c0b51be1a23d6f078e9750940c17d5ad2a437b79
ankitav456 <ankitav456@gmail.com>
2024-08-18 20:50:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// // script.js
+// document.querySelectorAll('.food-images img').forEach((img, index) => {
+//     img.addEventListener('mouseover', () => {
+//         img.style.transform = `translate(-50%, -50%) scale(1.5)`;
+//     });
+//     img.addEventListener('mouseout', () => {
+//         img.style.transform = `translate(-50%, -50%) scale(1)`;
+//     });
+// });
